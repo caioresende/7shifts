a349a3110918c37720c798dc03b6bf9d6158ab57
Caio Resende <caioresende@Caios-MacBook-Pro.local>
2018-06-24 15:42:47 -0300

    adding validation methods and optimizing getTimePunches

--- app/time-punches/TimePunchesService.js
+++ app/time-punches/TimePunchesService.js
@@ -1,17 +1,37 @@
-module.exports = function($q, $http) {
+module.exports = function($q, $http, DateService) {
     'njInject';
   
     var self = this;
   
-    var companyUrl = 'https://shiftstestapi.firebaseio.com/timePunches.json';
-  
-    this.getTimePunches = function() {
-      return $http.get(companyUrl).then(function(response) {
-        return $q.when(response);
-      });
+    var timePunchesUrl = 'https://shiftstestapi.firebaseio.com/timePunches.json';
+
+    this.timePunches = {};
+
+    this.isTimePunchValid = function(timePunch) {
+      //validates check ins and hourlyWage
+      if (timePunch.hourlyWage > 0) {
+        var clockedIn = new Date(timePunch.clockedIn);
+        var clockedOut = new Date(timePunch.clockedOut);
+        
+        //In case there is some clockIns later than clocksOut on the same day
+        if (clockedIn.getDate() == clockedOut.getDate() && clockedIn < clockedOut) {
+          return true;
+        }
+      }
+    };
+
+    this.splitTimePunchesByWeek = function(response) {
+      return $q.when(console.log(response));
     };
   
-    this.getTimePunches().then(function(response) {
-        console.log(response);
-    });
+    this.getTimePunches = function() {
+      if (!angular.equals(self.timePunches, {})) {
+        return $q.when(self.timePunches);
+      } else {
+        return $http.get(timePunchesUrl).then(function(response) {
+          self.timePunches = response.data;
+          return $q.when(self.timePunches);
+        });
+      }
+    };
   };
